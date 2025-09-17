3d3ba42c7d9d0e22d926b4cf2ad6b7c237593554
Ajay Sharma <aj@ajsharma.com>
2025-09-16 21:42:09 -0700

    simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,18 @@
+require 'simplecov'
+SimpleCov.start 'rails' do
+  add_filter '/test/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Jobs', 'app/jobs'
+  add_group 'Mailers', 'app/mailers'
+
+  track_files "app/**/*.rb"
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
