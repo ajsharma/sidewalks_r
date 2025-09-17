c03674ddd664f2bd1bbdb571158af678f58b1a39
Ajay Sharma <aj@ajsharma.com>
2025-09-16 21:03:17 -0700

    Fix migrations

--- db/migrate/20250917023648_add_timezone_to_users.rb
+++ db/migrate/20250917023648_add_timezone_to_users.rb
@@ -1,5 +1,5 @@
 class AddTimezoneToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :timezone, :string, default: 'America/Los_Angeles'
+    add_column :users, :timezone, :string, default: 'UTC'
   end
 end
