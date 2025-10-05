0b6e9b03e5cd610fc8c469d1a7bcad4cd95b483d
Ajay Sharma <aj@ajsharma.com>
2025-10-05 16:58:02 -0700

    Revert noise

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,8 +2,4 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
-
-  # TODO: Add accessibility testing with axe-core integration
-  # The axe-core-capybara gem is installed and available for future implementation
-  # Consider implementing custom accessibility assertions or using alternative gem
 end
